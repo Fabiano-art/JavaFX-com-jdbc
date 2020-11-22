8c6c8d0b00390c4535e343697128371c46611d1a
Fabiano-art <fabio.dub@outlook.com>
2020-11-21 22:32:52 -0300

    Classes utilitárias

---
+++ src/gui/util/Constraints.java
@@ -0,0 +1,22 @@
+package gui.util;
+
+import javafx.scene.control.TextField;
+
+public class Constraints {
+	
+	public static void setTextFieldInteger(TextField txt) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (newValue != null && !newValue.matches("\\d*")) {
+				txt.setText(oldValue);
+			}
+		});
+	}
+	
+	public static void setTextFieldMaxLenght(TextField txt, int max) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (newValue != null && newValue.length() > max) {
+				txt.setText(oldValue);
+			}
+		});
+	}
+}
