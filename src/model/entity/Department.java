9e115a963c2dc79676a123ec087f9a7dacf2219c
Fabiano-art <fabio.dub@outlook.com>
2020-11-23 18:23:06 -0300

    Lombok

--- src/model/entity/Department.java
+++ src/model/entity/Department.java
@@ -2,36 +2,19 @@ package model.entity;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Department implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
 	private String name;
-
-	public Department() {
-	}
-
-	public Department(Integer id, String name) {
-		this.id = id;
-		this.name = name;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 
 	@Override
 	public int hashCode() {
--- src/model/entity/Seller.java
+++ src/model/entity/Seller.java
@@ -3,6 +3,13 @@ package model.entity;
 import java.io.Serializable;
 import java.util.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
 public class Seller implements Serializable {
 	
 	
@@ -16,20 +23,6 @@ public class Seller implements Serializable {
 	
 	private Department department;
 	
-	public Seller() {
-		
-	}
-
-	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.email = email;
-		this.birthDate = birthDate;
-		this.baseSalary = baseSalary;
-		this.department = department;
-	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -41,55 +34,6 @@ public class Seller implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
-	}
-	
-	
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public Date getBirthDate() {
-		return birthDate;
-	}
-
-	public void setBirthDate(Date birthDate) {
-		this.birthDate = birthDate;
-	}
-
-	public Double getBaseSalary() {
-		return baseSalary;
-	}
-
-	public void setBaseSalary(Double baseSalary) {
-		this.baseSalary = baseSalary;
-	}
-
-	public Department getDepartment() {
-		return department;
-	}
-
-	public void setDepartment(Department department) {
-		this.department = department;
 	}
 
 	@Override
